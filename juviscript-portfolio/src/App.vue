86fa1294e9c90f011acf46606688f1cc3bd02bc8
Juvi <juvipana@gmail.com>
2026-02-28 13:23:49 -0500

    design: remove initial boilerplate code, imported fonts, and created placeholder screen

--- juviscript-portfolio/src/App.vue
+++ juviscript-portfolio/src/App.vue
@@ -1,30 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+ <div class="app">
+    <h1>juviscript.dev</h1>
+    <p>Portfolio loading...</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #008080; /* Windows 98 teal */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-family: 'Press Start 2P', monospace;
 }
 </style>
